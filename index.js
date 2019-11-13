6f53c5df05b721cffc7be239eea67dcf338fe156
Pratiksha <p.s.b.90909@gmail.com>
2019-11-13 21:17:36 +0530

    msg

--- index.js
+++ index.js
@@ -17,11 +17,7 @@ const port = parseInt(config.get("port"));
 
 //Use Middleware
 app.use(express.json());
-app.use("/admin",adminRoutes);
 app.use("/employees",empRoutes);
 
 
 //listen to port for requests
-app.listen(port, function(){
-    console.log("Server Started on port  " + port );
-})
